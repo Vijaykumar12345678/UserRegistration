79fecad1eaf919488fa0782582aba17294183b4f
vijay1 <vijaykumarynh7@gmail.com>
2024-09-16 15:42:47 +0530

    [Vijay] add: Python program for to validate the user details.

---
+++ py_test.py
@@ -0,0 +1,60 @@
+"""
+@Author:Vijay Kumar M N
+@Date: 2024-09-05
+@Last Modified by: Vijay Kumar M N
+@Last Modified: 2024-09-05
+@Title : python program for to validate the test cases.
+"""
+import pytest
+from UserRegister import is_valid_name,is_valid_email,is_valid_number,is_valid_password
+
+class TestfirstNameValidation:
+
+    def test_valid_first_name(self):
+        
+        assert is_valid_name("Smith")==True
+        assert is_valid_name("Jones")==True
+        assert is_valid_name("Brown")==True
+        assert is_valid_name('va')==False
+        assert is_valid_name('vijay')==False
+        
+    def test_valid_last_name(self):
+        
+        assert is_valid_name("Smith")==True
+        assert is_valid_name("Jones")==True
+        assert is_valid_name("Brown")==True
+        assert is_valid_name('va')==False
+        assert is_valid_name('vijay')==False
+    
+    def test_valid_mail(self):
+        assert is_valid_email("abc@yahoo.com")==True
+        assert is_valid_email("abc-100@yahoo.com")==True
+        assert is_valid_email("abc@.com.my")==False
+        assert is_valid_email(".abc@abc.com")==False
+    
+    def test_valid_number(self):
+        assert is_valid_number("91 8861912891")==True
+        assert is_valid_number("12 1234567890")==True
+        assert is_valid_number("8861912891")==False
+        assert is_valid_number("12 p61912891")==False
+    
+    def test_valid_password(self):
+        assert is_valid_password("Vijaykum1")==True
+        assert is_valid_password("V12345678")==True
+        assert is_valid_password("vijay kumar")==False
+        assert is_valid_password("vijay")==False
+
+    
+
+        
+    
+def main():
+    obj=TestfirstNameValidation()
+    obj.test_valid_first_name()
+    obj.test_valid_last_name()
+    obj.test_valid_mail()
+    obj.test_valid_number()
+    obj.test_valid_password()
+
+if __name__ == '__main__':
+    pytest.main()
